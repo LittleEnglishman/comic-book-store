1af9c347a3575c1e7868ad7eb76479a0854709c8
LittleEnglishman <nztomking@gmail.com>
2019-03-13 15:05:04 +1300

    Ver1.2

--- comic-book-store.py
+++ comic-book-store.py
@@ -10,18 +10,19 @@ from itertools import count
 class ComicBook:
     _ids = count(0)
 
-    def __init__(self,title,stock, price, description):
+    def __init__(self,title,stock, price, description, image):
         self.id = next(self._ids)
         self.title = title
         self.stock = stock
         self.price = price
         self.description = description
+        self.image = image
 
 #ver1.0 Creatioon of test data       
 comicBooks = [
-    ComicBook("Water Woman", 0, 19.99, "This book sucks, don't buy it."),
-    ComicBook("Not Water Woman", 13, 15.99, "This book is very good thus you should buy it."),
-    ComicBook("Wagon Wars", 26, 8.49, "In a world full of wagons, one wagon will destroy them all!")
+    ComicBook("Water Woman", 0, 19.99, "This book sucks, don't buy it.", "image/bookstore-cat.jpg"),
+    ComicBook("Not Water Woman", 13, 15.99, "This book is very good thus you should buy it.", "image/bookstore-cat.jpg"),
+    ComicBook("Wagon Wars", 26, 8.49, "In a world full of wagons, one wagon will destroy them all!", "image/bookstore-cat.jpg")
     ]
 
 #Images
@@ -45,6 +46,17 @@ def stylesheets(filename):
 def index():
     #need this function to attatch decorators above
     pass
+
+
+#Ver1.2 Book List Page
+@route('/book-list')
+@view('book-list')
+def book_list(): 
+    #make a dictionary of the comicBooks in stock
+    data = dict(comics = comicBooks)
+    return data
+
+
 
 #reloader = True breaks the code? Only at home PC though???? apparantly is a server issue
 run(host='localhost', port=8080, debug=True)
